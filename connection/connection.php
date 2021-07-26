05b31d77897087f8e8939a908f557eda91764da7
Muhammad Naim b. Abd Rahim <63232585+z2gamma@users.noreply.github.com>
2021-07-26 23:50:27 +0800

    Add files via upload

--- connection/connection.php
+++ connection/connection.php
@@ -1,1 +1,16 @@
+<?php
+session_start();
+$pelayan = "localhost";
+$penggunapelayan = "root";
+$katalaluanpelayan = "";
+$pangkalandata = "naim";
 
+$sambungan = mysqli_connect($pelayan, $penggunapelayan, $katalaluanpelayan, $pangkalandata);
+
+if(!$sambungan){
+	echo "Gagal membuat sambungan kerana: ".mysqli_connect_error($sambungan);
+}else{
+
+}
+error_reporting(E_ALL ^ E_NOTICE);
+?>
